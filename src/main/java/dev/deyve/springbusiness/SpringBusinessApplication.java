20d58aef7677df880410ef4deeb174d332ef14b2
Deyve Vieira <deyvedvm@hotmail.com>
2020-05-23 20:02:26 -0300

    Initial commit

---
+++ src/main/java/dev/deyve/springbusiness/SpringBusinessApplication.java
@@ -0,0 +1,13 @@
+package dev.deyve.springbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/deyve/springbusiness/SpringBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.deyve.springbusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
